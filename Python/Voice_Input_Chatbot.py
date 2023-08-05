e182cf8f96e4a0306055974844a6b27ef884a4fd
TianTian Fan <slamacowcrea@gmail.com>
2023-08-05 17:01:59 -0400

    Python and Unity

---
+++ Python/Voice_Input_Chatbot.py
@@ -0,0 +1,78 @@
+import openai
+#import pyttsx3
+import speech_recognition as sr
+import threading
+from flask import Flask, request
+
+
+openai.api_key = ""
+
+#engine = pyttsx3.init()
+#voices = engine.getProperty('voices')
+app = Flask(__name__)
+
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        print('error')
+
+ 
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine = "text-davinci-003",
+        #prompt="You are an AI assistant, your name is Kevin, you work at the game lab of ontario Tech University",
+        prompt=prompt,
+        max_tokens=4000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    global chat
+    chat = response["choices"][0]["text"]
+    return chat
+
+@app.route('/')
+def upload():
+    return chat
+
+
+#def speak_text(text):
+    engine.setProperty('voice', voices[0].id)
+    engine.say(text)
+    engine.runAndWait()
+
+
+def main():
+    threading.Thread(target=lambda: app.run(use_reloader=False)).start()
+    while True:
+        print("Say 'wake up chat' to start recording your question")
+        with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
+            try:
+                transcription = recognizer.recognize_google(audio)
+                if transcription.lower() == "wake up chat":
+                    filename = "input.wav"
+                    print("Say your question")
+                    with sr.Microphone() as source:
+                        recognizer = sr.Recognizer()
+                        source.pause_threshold = 1
+                        audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
+                        with open(filename, "wb") as f:
+                            f.write(audio.get_wav_data())     
+                    text = transcribe_audio_to_text(filename)
+                    if text:
+                        print(f"You said: {text}")
+                        response = generate_response(text)
+                        print(f"GPT-3 says: {response}")
+                        #speak_text(response)
+                       
+            except Exception as e:
+                print("error: {}".format(e))
+                
+if __name__ == "__main__":
+    main()
